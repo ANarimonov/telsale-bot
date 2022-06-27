5d1627333dc8d085988662ac83eb803ed1d9e695
Abdurrohman Narimonov <abdurrohmannarimonov@gmail.com>
2022-06-27 18:38:38 +0500

    -Bug fixes and other minor improvements

--- src/main/java/com/anarimonov/phonesalebot/model/Brand.java
+++ src/main/java/com/anarimonov/phonesalebot/model/Brand.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @NoArgsConstructor
@@ -13,6 +14,7 @@ import javax.persistence.Entity;
 @Entity(name = "brands")
 public class Brand extends AbsEntity {
 
+    @Column(columnDefinition = "text")
     private String message;
 
     public Brand(String name) {
