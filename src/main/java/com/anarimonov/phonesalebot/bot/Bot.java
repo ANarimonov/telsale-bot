908e5ae9a4a9a38dd200aaeac68e4de575c8a404
Abdurrohman Narimonov <abdurrohmannarimonov@gmail.com>
2022-06-27 12:27:22 +0500

    -Bug fixes and other minor improvements

--- src/main/java/com/anarimonov/phonesalebot/bot/Bot.java
+++ src/main/java/com/anarimonov/phonesalebot/bot/Bot.java
@@ -677,7 +677,7 @@ public class Bot extends TelegramLongPollingBot {
             sendTextMessage(userActivity.setStep(0), "Brendi:" + productDto.getBrand() + "\n" +
                     "Modeli:" + productDto.getModel() + "\n" +
                     "\n" +
-                    (productDto.getBrand().equalsIgnoreCase("iphone") ? "Batareyka foizi:" + productDto.getBatteryCapacity() : "") +
+                    (productDto.getBrand().equalsIgnoreCase("iphone") ? "Batareyka foizi:" + productDto.getBatteryCapacity() +"\n": "") +
                     "Korobka dokumenti:" + (productDto.isDocuments() ? "Bor" : "Yo'q") + "\n" +
                     "Rangi:" + productDto.getColor() + "\n" +
                     "Xotirasi:" + productDto.getStorage() + "\n" +
@@ -689,7 +689,7 @@ public class Bot extends TelegramLongPollingBot {
             sendTextMessage(userActivity.setStep(0), "Бренд:" + productDto.getBrand() + "\n" +
                     "Модель:" + productDto.getModel() + "\n" +
                     "\n" +
-                    (productDto.getBrand().equalsIgnoreCase("iphone") ? "Процент батареи:" + productDto.getBatteryCapacity() : "") +
+                    (productDto.getBrand().equalsIgnoreCase("iphone") ? "Процент батареи:" + productDto.getBatteryCapacity() +"\n": "") +
                     "Коробка и документ:" + (productDto.isDocuments() ? "Есть" : "Нет") + "\n" +
                     "Цвет:" + productDto.getColor() + "\n" +
                     "Память:" + productDto.getStorage() + "\n" +
